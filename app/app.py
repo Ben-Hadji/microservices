ff3ff4674bfd09b78e032fe72e9bbc4336de661a
Ben-Hadji <benhadji269@gmail.com>
2023-10-18 15:07:22 +0200

    third

--- app/app.py
+++ app/app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host='redis-container', port=6379)
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    counter = str(redis.get('hits'), 'utf-8')  # Corrigé cette ligne
-    return "Welcome to my webpage! This webpage has been viewed " + counter + " time(s)"
+    return ' - - - This basic web page has been viewed {} time(s) - - -'.format(redis.get('hits'))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
